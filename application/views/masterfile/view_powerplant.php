01766427918afa22728aa56ad059b4d69704b2de
ProjectPixel <jasonflor.cenpri@gmail.com>
2019-12-09 11:13:43 +0800

    view pplant

---
+++ application/views/masterfile/view_powerplant.php
@@ -0,0 +1,108 @@
+<div class="page-wrapper">
+    <div class="container-fluid">
+        <div class="row page-titles">
+            <div class="col-md-7 align-self-center text-right">
+                <div class="d-flex  align-items-center"> <!-- justify-content-end -->
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>masterfile/dashboard/">Dashboard</a></li>
+                        <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>masterfile/powerplant_list/">Power Plant</a></li>
+                        <li class="breadcrumb-item active">View</li>
+                        <li class="breadcrumb-item">
+                        </li>
+                    </ol>
+                </div>
+            </div>
+            <div class="col-md-5 align-self-center">
+            </div>            
+        </div>
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                    <h5><small>Facility Name</small><br>Facility Name</h5>
+                                    <h5><small>Type </small><br>Type</h5>
+                                    <h5><small>Sub Type </small><br>sub Type</h5>
+                                    <h5><small>Operator </small><br>Operator</h5>
+                                    <h5><small>Participant ID </small><br>Participant ID</h5>
+                                    <h5><small>Region </small><br>Region</h5>
+                                    <h5><small>Region ID </small><br>Region ID </h5>
+                                    <h5><small>Municipality/Province </small><br>Municipality/Province </h5>
+                                    <h5><small>Location </small><br>Location </h5>
+                                    <h5><small>Capacity Installed (MW) </small><br>Capacity Installed (MW) </h5>
+                                    <h5><small>Capacity Dependable (MW) </small><br>Capacity Dependable (MW) </h5>
+                                    <h5><small>Number of Units </small><br>Number of Units </h5>
+                                    <h5><small>IPPA </small><br>IPPA </h5>
+                                    <h5><small>Fit Approved </small><br>Fit Approved </h5>
+                                    <h5><small>Owner Type </small><br>Owner Type </h5>
+                                    <h5><small>Type of Contract </small><br>Type of Contract </h5>
+                                    <h5><small>Status </small><br>Status </h5>
+                            </div>
+                            <div class="col-lg-6">
+                                <div style="overflow-y: scroll;height: 750px;padding: 20px;background: #efefef">
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                    <div class="card shadow bor-radius20 p-20">
+                                        <h5><small>Resource ID</small><br>Resource ID</h5>
+                                        <h5><small>Date Commisioned/Commercial</small><br>Resource ID</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
