c1076f93cb4a35c4b5b189b88e321b70707ba189
stephinesev <stephineseverino.cenpri@gmail.com>
2019-12-17 14:09:03 +0800

    Fix loop in powerplant second

--- application/views/masterfile/add_powerplant_second.php
+++ application/views/masterfile/add_powerplant_second.php
@@ -31,32 +31,22 @@
                                 <form method = "POST" action="<?php echo $url; ?>">
                                     <!-- loop -->
                                     <?php 
-                                        if(empty($update)){ 
-                                            for($x=1;$x<=$unit_number;$x++){ 
+                                        for($x=0;$x<$unit_number;$x++){ 
                                     ?>
                                         <div class="card shadow bor-radius20 p-20">
                                             <div class="form-group">
-                                                <input type="text" placeholder="Resource ID" class="form-control" name="resource_id<?php echo $x; ?>">
+                                                <input type="text" placeholder="Resource ID" class="form-control" name="resource_id<?php echo $x; ?>" value = "<?php echo (!empty($resource[$x]['resource_id'])) ? $resource[$x]['resource_id'] : ''; ?>">
                                             </div>
-                                            <input type="date" placeholder="Date Commisioned/Commercial" class="form-control" name="com_date<?php echo $x; ?>">
-                                        </div>
-                                    <?php } $count = $x-1; }else { 
-                                        $x=1;
-                                        foreach($resource AS $r){
-                                    ?>
-                                        <div class="card shadow bor-radius20 p-20">
                                             <div class="form-group">
-                                                <input type="text" placeholder="Resource ID" class="form-control" name="resource_id<?php echo $x; ?>" value = "<?php echo (isset($update)) ? $r['resource_id'] : ''; ?>">
+                                                <input type="date" placeholder="Date Commisioned/Commercial" class="form-control" name="com_date<?php echo $x; ?>" value = "<?php echo (!empty($resource[$x]['date_commissioned'])) ? $resource[$x]['date_commissioned'] : ''; ?>">
                                             </div>
-                                            <input type="date" placeholder="Date Commisioned/Commercial" class="form-control" name="com_date<?php echo $x; ?>" value = "<?php echo (isset($update)) ? $r['date_commissioned'] : ''; ?>">
+                                            <input type="hidden" class="form-control" name="ppr_id<?php echo $x; ?>" value = "<?php echo (!empty($resource[$x]['ppr_id'])) ? $resource[$x]['ppr_id'] : ''; ?>">
                                         </div>
-                                        <input type="hidden" class="form-control" name="ppr_id<?php echo $x; ?>" value = "<?php echo $r['ppr_id']; ?>">
-                                    <?php $x++; } $count = $x-1; } ?>
+                                    <?php } ?>
                                     <!-- loop -->
-                                    <input type="hidden" class="form-control" name="count" value = "<?php echo $count; ?>">
+                                    <input type="hidden" class="form-control" name="count" value = "<?php echo $x; ?>">
                                     <input type="hidden" class="form-control" name="powerplant_id" value = "<?php echo $powerplant_id; ?>">
                                     <div class="form-group">
-                                        <!-- <a href="<?php echo base_url(); ?>masterfile/add_powerplant_second" class="btn btn-info-alt btn-block">Submit</a> -->
                                         <input type="submit" name="submit" class="btn btn-info-alt btn-block" value = "Submit">
                                     </div>
                                 </form>
